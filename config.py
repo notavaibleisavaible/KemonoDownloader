9c36983550c45f3c1a60093c527eeb59956caf1c
notavaibleisavaible <148712868+notavaibleisavaible@users.noreply.github.com>
2024-01-21 18:41:31 -0300

    wrong comment

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # PUT A '/' AT THE END! ! !
 # PUT A '/' AT THE END! ! !
 # PUT A '/' AT THE END! ! !
-# PUT A '/' AT THE END! ! ! 4287104
-# DOWNLOAD_DIR = '<change in config file>'
+# PUT A '/' AT THE END! ! !
+DOWNLOAD_DIR = '<change in config file>'
 
 
 #MAXIMUM OF SIMULTANEOUS DOWNLOADS, WOULDNT RECOMMEND MORE THAN 3 (the server rejects)
